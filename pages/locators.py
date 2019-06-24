05e7206814a2fa795dad6460e1a2be6df7190f43
Frezer <i.e.lopatin@gmail.com>
2019-06-24 16:28:02 +0500

    add Page Obgect for login page and modify locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,17 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators(object):
+
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(object):
+
+    LOGIN_EMAIL = (By.ID, 'id_login-username')
+    LOGIN_PASSWORD = (By.ID, 'id_login-password')
+    LOGIN_ACCEPT_BUTTON = (By.NAME, 'login_sumit')
+    LOGIN_FORGOTTEN_PASS_LINK = (By.PARTIAL_LINK_TEXT, 'password-reset')
+
+    REGISTER_EMAIL = (By.ID, 'id_registration-email')
+    REGISTER_PASSWORD = (By.ID, 'id_registration-password1')
+    REGISTER_REPEATED_PASSWORD = (By.ID, 'id_registration-password2')
+    REGISTER_ACCEPT_BUTTON = (By.NAME, 'registration_submit')
---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.url, 'login url not contains "login" substring'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "login e-mail field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASWORD), "login password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_ACCEPT_BUTTON), "login accept button is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORGOTTEN_PASS_LINK), "login link for forgotten password is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL), "registration e-mail field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_PASWORD), "registration password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_REPEATED_PASSWORD), "registration repeated password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_ACCEPT_BUTTON), "registration accept button is not presented"
